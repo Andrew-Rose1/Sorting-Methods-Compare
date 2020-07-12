2426acb33815d9839714b923d71581c3dd31debb
Qiang Hao <qiang.hao@wwu.edu>
2020-07-11 17:45:06 -0700

    Initial commit

---
+++ src/main/java/SortingPack.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class SortingPack {
+    // just in case you need tis method for testing
+    public static void main(String[] args) {
+        // something
+    }
+
+    // implementation of insertion sort
+    // parameters: int array unsortedArr
+    // return: sorted int array
+    public static int[] insertionSort(int[] unsortedArr) {
+        // to be removed
+        return unsortedArr;
+    }
+
+    // implementation of quick sort
+    // parameters: int array unsortedArr
+    // return: sorted int array
+    public static int[] quickSort(int[] unsortedArr) {
+        // to be removed
+        return unsortedArr;
+    }
+
+    // implementation of merge sort
+    // parameters: int array unsortedArr
+    // return: sorted int array
+    public static int[] mergeSort(int[] unsortedArr) {
+        // to be removed
+        return unsortedArr;
+    }
+
+    // implementation of heap sort
+    // parameters: int array unsortedArr
+    // return: sorted int array
+    public static int[] heapSort(int[] unsortedArr) {
+        // to be removed
+        return unsortedArr;
+    }
+    // you are welcome to add any supporting methods
+}
---
+++ src/main/java/SortingTest.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class SortingTest{
+	public static void main(String[] args) {
+
+	}
+
+	// this method generate a random int array with the length as size
+	// each random int should be in [0, 10000]
+	// parameters: size
+	// return data: int[]
+	public int[] randomArray(int size) {
+		// remove these two lines
+		int[] arr = {0};
+		return arr;
+	}
+
+	// this method counts the time it takes to sort arr using sortingAlgorithm
+	// parameters: sortingAlgorithm and int array
+	// return data: double
+	public double sortingTime(String sortingAlgorithm, int[] arr) {
+		// remove this line
+		return 0.0;
+	}
+
+  // you are welcome to add any supporting methods
+}
